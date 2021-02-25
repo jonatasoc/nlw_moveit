06bdcbca4da4987d041572ef9a4a492c3046640c
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-02-24 21:12:01 -0300

    Add title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+import Head from "next/head";
+
 import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
+
 import { GlobalStyle } from "../styles/global";
 import { Container } from "./styles";
 
 export default function Home() {
   return (
     <Container>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <GlobalStyle />
       <ExperienceBar />
 
