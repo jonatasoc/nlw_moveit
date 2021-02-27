286be1e0655b1fc10f91558baefd4e9557788b2b
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-02-27 17:59:09 -0300

    Add Countdowncontext

---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,83 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+import { ChallengesContext } from './ChallengesContext';
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
+
+interface CountdownContextData {
+  startCountdown: () => void;
+  resetCountdown: () => void;
+  time: number;
+  minutes: number;
+  seconds: number;
+  isActive: boolean;
+  hasFinished: boolean;
+}
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+let countdownTimeout: NodeJS.Timeout;
+
+export const CountdownProvider = ({ children }: CountdownProviderProps) => {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function startCountdown() {
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.1 * 60);
+    setHasFinished(false);
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
+    }
+  }, [isActive, time]);
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        time,
+        minutes,
+        seconds,
+        isActive,
+        hasFinished,
+        startCountdown,
+        resetCountdown,
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  );
+};
