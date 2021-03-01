bbc01c368af359c4a08e41781575f1c323783d63
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-02-28 23:59:43 -0300

    Add LevelUpModal

---
+++ src/components/LevelUpModal/LevelUpModal.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { ChallengesContext } from '../../contexts/ChallengesContext';
+
+import { Container, Overlay } from './styles';
+
+const LevelUpModal: React.FC = () => {
+  const { level, closeLevelUpModal } = useContext(ChallengesContext);
+  return (
+    <Overlay>
+      <Container>
+        <header>{level}</header>
+
+        <strong>Parabéns</strong>
+        <p>Você alcançou um novo level.</p>
+
+        <button type="button" onClick={closeLevelUpModal}>
+          <img src="/icons/close.svg" alt="Fechar modal" />
+        </button>
+      </Container>
+    </Overlay>
+  );
+};
+
+export default LevelUpModal;
