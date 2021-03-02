1d64d970494f10f6217a321838d3dcce83dd663e
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-03-02 17:19:44 -0300

    Add styled-components function to get styles before rendering the page

---
+++ src/pages/_document.js
@@ -0,0 +1,41 @@
+import Document, { Html, Main, Head, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+
+    // Step 2: Retrieve styles from components in the page
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />),
+    );
+
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+
+    // Step 4: Pass styleTags as a prop
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
+          />
+          {/* Step 5: Output the styles in the head  */}
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
